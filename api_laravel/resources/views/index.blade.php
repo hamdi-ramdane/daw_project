24cd3c604a6a55ca66f9dbb10e070b41184f2dbd
hamdi ramdane <g3.hamdi.ramdane@gmail.com>
2023-11-16 18:15:33 +0100

    transition from a webapp to just an API

--- api_laravel/resources/views/index.blade.php
+++ api_laravel/resources/views/index.blade.php
@@ -3,12 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/main.css">
-    @yield("styles")
-    <title>@yield("title")</title>
+    <link rel="stylesheet" href="api.css">
+    <title>Pixel Recovery API</title>
 </head>
 <body>
-    <a href="/" class="logo">Home</a>
-    @yield('content')
+    <h1>Pixel Recovery API</h1> 
+    <h3>{{ $_SERVER['HTTP_HOST'] }}/api/</h3>
 </body>
 </html>
--- api_laravel/routes/web.php
+++ api_laravel/routes/web.php
@@ -2,24 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-$cntls = 'App\Http\Controllers\\';
+$path = 'App\Http\Controllers\\';
 
-Route::get('/', $cntls.'HomeController@index');
-
-Route::get('/auth', $cntls.'LoginController@index');
-Route::get('/auth/register', $cntls.'RegisterController@index');
-
-Route::get('/quiz', $cntls.'QuizController@index');
-Route::get('/quiz/addiction', $cntls.'QuizController@index');
-Route::get('/quiz/depression', $cntls.'QuizController@index');
-Route::get('/quiz/adhd', $cntls.'QuizController@index');
-Route::get('/quiz/insomnia', $cntls.'QuizController@index');
-
-Route::get('/profile', $cntls.'ProfileController@index');
-
-Route::get('/admin', $cntls.'AdminController@index');
-
-// Post Routes ============================================
-
-Route::post('/auth',$cntls.'LoginController@check');
-Route::post('/auth/register',$cntls.'RegisterController@store');
+Route::get("/",function(){return view('index');});
